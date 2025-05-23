0b122a26294b4c4f6f4d86e19b10c49ea09fa57b
pablocasta1980 <pacastanog@uqvirtual.edu.co>
2025-05-22 19:12:27 -0500

    actualizacion front

--- src/app/componentes/cambiar-contrasena/cambiar-contrasena.component.ts
+++ src/app/componentes/cambiar-contrasena/cambiar-contrasena.component.ts
@@ -6,6 +6,7 @@ import { CommonModule } from '@angular/common';
 import { AuthService } from '../../servicios/auth.service';
 import { TokenService } from '../../servicios/token.service';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-cambiar-contrasena',
@@ -54,31 +55,38 @@ export class CambiarContrasenaComponent implements OnInit {
     );
   }
 
-  cambiarContrasena() {
-    this.cambioPasswordDto.token = this.cambioPassword.token;
-    this.cambioPasswordDto.email = this.cambioPassword.correo;
-    this.cambioPasswordDto.nuevaPassword = this.cambioPassword.nuevaContrasena;
+cambiarContrasena() {
+  this.cambioPasswordDto.token = this.cambioPassword.token;
+  this.cambioPasswordDto.email = this.cambioPassword.correo;
+  this.cambioPasswordDto.nuevaPassword = this.cambioPassword.nuevaContrasena;
 
-    console.log('Token enviado:', this.cambioPasswordDto);
+  console.log('Token enviado:', this.cambioPasswordDto);
 
-    this.authService.cambiarContraseña(this.cambioPasswordDto).subscribe({
-      next: (data) => {
-        
-        console.log('Contraseña modificada correctamente');
-      },
-      error: (error) => {
-        console.error(JSON.stringify(error));
-        
-        if (error.status === 200) {
-          alert('Contraseña modificada correctamente');
+  this.authService.cambiarContraseña(this.cambioPasswordDto).subscribe({
+    next: (data) => {
+      console.log('Contraseña modificada correctamente');
+      
+      Swal.fire({
+        icon: 'success',
+        title: '¡Éxito!',
+        text: 'Contraseña modificada correctamente',
+        confirmButtonText: 'Aceptar'
+      }).then(() => {
+        this.router.navigate(['/login']);
+      });
+    },
+    error: (error) => {
+      console.error(JSON.stringify(error));
 
-          this.router.navigate(['/login']);
-        }
-
-        if (error.status === 500) {
-          console.error('Error en el servidor');
-        } 
-      },
-    });
-  }
+      if (error.status === 500) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error del servidor',
+          text: 'Ocurrió un problema al cambiar la contraseña.',
+          confirmButtonText: 'Cerrar'
+        });
+      }
+    },
+  });
+}
 }
